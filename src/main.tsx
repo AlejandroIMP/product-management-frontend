61565789f57384ad11de0b48684ea746212f94ef
Alejandro Sunuc <alejandrosunuc@gmail.com>
2025-09-22 19:09:52 -0600

    feat: restructure routing in main.tsx and clean up index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Products from './pages/products'
+import ProductDetail from './pages/products/:id'
+import NotFound from './pages/NotFound'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Products />} />
+        <Route path='products'>
+          <Route path=':id' element={<ProductDetail />} />
+        </Route>
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
