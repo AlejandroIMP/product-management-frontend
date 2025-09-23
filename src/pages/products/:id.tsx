0aa33191d6442ef43780adc7c76b922739ca7f0c
Alejandro Sunuc <alejandrosunuc@gmail.com>
2025-09-23 12:47:25 -0600

    refactor: standardize string quotes in :id.tsx

--- src/pages/products/:id.tsx
+++ src/pages/products/:id.tsx
@@ -1,5 +1,5 @@
-import { Product } from "../../features/Management/Products/components/Product"
-import { Layout } from "../../Layouts/layout"
+import { Product } from '../../features/Management/Products/components/Product'
+import { Layout } from '../../Layouts/layout'
 function ProductDetail() {
   return (
     <Layout>
