fe3e5e49ef6f719136081c17eb265c532ff194e3
Alejandro Sunuc <alejandrosunuc@gmail.com>
2025-09-23 12:47:15 -0600

    refactor: standardize string quotes in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path='products'>
           <Route path=':id' element={<ProductDetail />} />
         </Route>
-        <Route path="/*" element={<NotFound />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
