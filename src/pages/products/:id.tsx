b4ebec0bd9bd03c7bf9e7672f4316def1bc1fc47
Alejandro Sunuc <alejandrosunuc@gmail.com>
2025-09-22 19:10:15 -0600

    feat: add ProductDetail and ProductsList components for product display

---
+++ src/pages/products/:id.tsx
@@ -0,0 +1,11 @@
+import { Product } from "../../features/Management/Products/components/Product"
+import { Layout } from "../../Layouts/layout"
+function ProductDetail() {
+  return (
+    <Layout>
+      <Product />
+    </Layout>
+  )
+}
+
+export default ProductDetail
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,11 @@
+import { ProductsList } from '../../features/Management/Products'
+import { Layout } from '../../Layouts/layout'
+function App() {
+  return (
+    <Layout>
+      <ProductsList />
+    </Layout>
+  );
+}
+
+export default App
