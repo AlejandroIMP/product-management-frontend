4ef4042eceb7c6bb1de9b156769852315e4c3ac1
Alejandro Sunuc <alejandrosunuc@gmail.com>
2025-09-23 12:47:20 -0600

    refactor: standardize string quotes in NotFound component

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,13 +1,13 @@
-import { Layout } from "../../Layouts/layout";
+import { Layout } from '../../Layouts/layout';
 
 
 function NotFound() {
   return(
     <Layout>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-8">Page Not Found</p>
-        <a href="/" className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 lg:px-8 lg:py-4 lg:text-lg">
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <h1 className='text-6xl font-bold text-gray-800 mb-4'>404</h1>
+        <p className='text-xl text-gray-600 mb-8'>Page Not Found</p>
+        <a href='/' className='px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 lg:px-8 lg:py-4 lg:text-lg'>
           Go to Home
         </a>
       </div>
